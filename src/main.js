222b143ed7f7c4a8211c42e36db89555549a5c0b
kite <943443868@qq.com>
2019-12-06 09:46:11 +0800

    reset/border.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/res/css/reset.css'
+import './assets/res/css/border.css'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
